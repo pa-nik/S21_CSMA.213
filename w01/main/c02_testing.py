407e36a0fa15c68ff3e9167e5009d78964549f11
vadev <bigballer131@gmail.com>
2021-01-14 11:31:57 -0800

    testing out numpy

---
+++ w01/main/c02_testing.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+a = np.array([1, 4, 2, 5, 3])
+print(a)
+a = np.zeros(10, dtype=int)
+print(a)
+a = np.ones((3, 5), dtype=float)
+print(a)
+a = np.linspace(0,1,5)
+print(a)
+x = np.array([1,2,3])
+y = np.array([4,5,6])
+print(x+y)
+print(x*2)
+print(np.add(x,y))
+print(np.multiply(x,2))
+
+
