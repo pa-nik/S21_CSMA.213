96881faeb0124a675705801bec12b8a63ed63bf3
nik <nikitapashenkov@gmail.com>
2021-02-09 10:48:16 -0800

    binarize data

--- w05/preprocessing_test.py
+++ w05/preprocessing_test.py
@@ -8,7 +8,12 @@ input_data = np.array([[5.1, -2.9, 3.3],
 print('input data:')
 print(input_data)
 
-# normalize data between -1 and 1
+# binarize input data
+data_binarized = preprocessing.Binarizer(threshold=2.1).transform(input_data)
+print('binarized input data:')
+print(data_binarized)
+
+# normalize input data between -1 and 1
 data_normalized_l1 = preprocessing.normalize(input_data, norm='l1')
 print('normalized input data:')
 print(data_normalized_l1)
