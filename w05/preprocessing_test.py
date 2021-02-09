9c3397c623f804c94b26d73cf6ba79ba4c671caf
nik <nikitapashenkov@gmail.com>
2021-02-09 10:46:32 -0800

    normalize input data

--- w05/preprocessing_test.py
+++ w05/preprocessing_test.py
@@ -5,3 +5,10 @@ input_data = np.array([[5.1, -2.9, 3.3],
                        [-1.2, 7.8, -6.1],
                        [3.9, 0.4, 2.1],
                        [7.3, -9.9, -4.5]]) 
+print('input data:')
+print(input_data)
+
+# normalize data between -1 and 1
+data_normalized_l1 = preprocessing.normalize(input_data, norm='l1')
+print('normalized input data:')
+print(data_normalized_l1)
